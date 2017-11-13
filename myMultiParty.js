5ce0580e57180cbfcda1130c4b5a4431a7bb59e5
keisuna <zaq12wsx594@yahoo.co.jp>
2017-11-13 17:46:39 +0900

    beta

--- myMultiParty.js
+++ myMultiParty.js
@@ -53,7 +53,7 @@ function Enter(key, room, role)
         gameInstance.SendMessage('ProjectManager', 'ExitPartner');
     }).on('message', function (message)
     {
-        console.log(message["date"]);
+        console.log(message["data"]);
         gameInstance.SendMessage('Partner', 'GetData', message["data"]);
     });
 
